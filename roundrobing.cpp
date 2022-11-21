79d0d415f454691fff7c95fe5f263c18ea4632e0
Pranshu Kala <pranshukalagmer2003@gmail.com>
2022-11-21 14:39:25 +0530

    m

--- roundrobing.cpp
+++ roundrobing.cpp
@@ -1,23 +1,18 @@
-#include <bits/stdc++.h>
-#include <algorithm>
-
+#include <iostream>
 using namespace std;
-
 class process
 {
 public:
+    int pid;
     int burst_time;
     int arrival_time = 0;
 };
-
-void compute_rr(process parray[], int n, int time)
-{
+void compute_rr(process parray[], int n, int time){
     int burst_time_copy[n];
     int wt[n];
     int tat[n];
     int track_time = 0;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         burst_time_copy[i] = parray[i].burst_time;
     }
     do
@@ -25,8 +20,7 @@ void compute_rr(process parray[], int n, int time)
         bool exit = true;
         for (int i = 0; i < n; i++)
         {
-            if (burst_time_copy[i] > 0)
-            {
+            if (burst_time_copy[i] > 0){
                 exit = false;
                 if (burst_time_copy[i] > time)
                 {
@@ -41,16 +35,15 @@ void compute_rr(process parray[], int n, int time)
                 }
             }
         }
-
-        if (exit == true)
-        {
+        if (exit == true){
             break;
         }
 
-    }while(true);
+    }while (true);
+
     for (int i = 0; i < n; i++)
     {
-        tat[i]= wt[i] + parray[i].burst_time;
+        tat[i] = wt[i] + parray[i].burst_time;
     }
     int totalwt = 0, totaltat = 0;
     for (int i = 0; i < n; i++)
@@ -61,11 +54,22 @@ void compute_rr(process parray[], int n, int time)
     {
         totaltat += tat[i];
     }
-    cout << "avg wt :" << (float)totalwt / (float)n << "\n";
+    cout << "Process ID \t Burst Time\t Waiting Time" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << parray[i].pid << " \t\t " << parray[i].burst_time << " \t\t " << wt[i] << " \t\t "
+             << "\n";
+    }
+    cout << "turn around time" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << tat[i] << " \t\t "
+             << "\n";
+    }
+
+    cout << "\n";
+    cout << "avg waiting time :" << (float)totalwt / (float)n << "\n";
     cout << "avg tat :" << (float)totaltat / (float)n;
-    
-
-
 }
 
 int main()
@@ -76,6 +80,8 @@ int main()
     process parray[n];
     for (int i = 0; i < n; i++)
     {
+        cout << "Enter process id of " << i + 1 << " process :";
+        cin >> parray[i].pid;
         cout << "Enter burst time of " << i + 1 << " process :";
         cin >> parray[i].burst_time;
     }
